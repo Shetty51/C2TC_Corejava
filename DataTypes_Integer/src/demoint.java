4e1c31e30c90b6944e9cdb031e41606a18b00d3c
DELL <DELL@Shetty>
2022-10-13 06:57:01 +0530

    First commit

---
+++ DataTypes_Integer/src/demoint.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author DELL
+ *
+ */
+public class demoint {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
