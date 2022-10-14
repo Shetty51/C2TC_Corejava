dcfeae6b26cb3d27a386220afec6a1e309ddb4a3
DELL <DELL@Shetty>
2022-10-14 08:26:59 +0530

    First commit

---
+++ Assignment_Operator/src/demoassignment.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author DELL
+ *
+ */
+public class demoassignment {
+
+
+	public static void main(String[] args) {
+		
+
+			int x = 4;
+			x+=5;      // x=x+5;
+			//x-=5, x=x-5;
+			//x*=5, x=x*5;
+			//x/=5, x=x/5;
+			//x%=5, x=x%5;
+			System.out.println(x);
+		
+		
+	}
+
+}
