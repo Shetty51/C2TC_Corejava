2d0a1990ad802637d8d757c46996001001725d86
DELL <DELL@Shetty>
2022-10-14 08:20:38 +0530

    First commit

---
+++ BooleanUnicode/src/demo_bool.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author DELL
+ *
+ */
+public class demo_bool {
+
+	public static void main(String[] args) {
+		
+		
+		boolean var = false;
+		System.out.println(var);
+		char var1 = '\u0047';
+		System.out.println(var1);
+	}
+
+}
