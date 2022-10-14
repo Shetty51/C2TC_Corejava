c15fbd27e18bbba314fbfe0bd1c4885ae1b037d0
DELL <DELL@Shetty>
2022-10-14 07:56:24 +0530

    First commit

---
+++ TypeCasting/src/demotypecast.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author DELL
+ *
+ */
+public class demotypecast {
+
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		float f = 10.532f;
+		long l = (long)f;
+		System.out.println(l);
+	}
+
+}
