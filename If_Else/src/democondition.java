588deb0802c5bbc820326a014b0f2ac766858b8e
DELL <DELL@Shetty>
2022-10-14 08:55:07 +0530

    First commit

---
+++ If_Else/src/democondition.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+
+/**
+ * @author DELL
+ *
+ */
+public class democondition {
+
+
+	public static void main(String[] args) {
+		
+
+		int x = 5;	
+		
+		  if (x > 5) 
+		  {
+		  	System.out.println("X is greater than 5"); 
+		   }
+		  else 
+		  {
+		  System.out.println("X is smaller than 5"); 
+		  } 
+		 }
+		
+		
+	}
+
