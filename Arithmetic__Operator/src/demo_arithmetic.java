3e86869ce26310968ae5b821546bd0bcac6bd30f
DELL <DELL@Shetty>
2022-10-14 08:31:41 +0530

    First commit

---
+++ Arithmetic__Operator/src/demo_arithmetic.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author DELL
+ *
+ */
+public class demo_arithmetic {
+
+	public static void main(String[] args) {
+		
+		int x=10, y=20;
+		System.out.println(x+y);
+		System.out.println(--x);
+		System.out.println(++y);
+
+	}
+
+}
