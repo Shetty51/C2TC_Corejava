e5fae9ff2823cb0a1c4a9ab499ad3ea982d4e797
DELL <DELL@Shetty>
2022-10-14 07:46:24 +0530

    First commit

---
+++ DataTypes_Integer/src/demoint.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+
+/**
+ * @author DELL
+ *
+ */
+public class demoint {
+
+	public static void main(String[] args) {
+	
+		
+		
+		int max = 2147483647;
+		int min = -2147483648;
+		short shortMax = 32761;
+		short shortmin = -32768;
+		long longMax = 9223372036845775807L;
+		long longMin = -9223372036845775808L;
+		byte  byteMax = 127;
+		byte byteMin = -128;
+		
+		
+
+		
+		
+		
+		
+	}
+
+}
