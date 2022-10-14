e15a134a06e304f09a0346dd94b965e1f586c286
DELL <DELL@Shetty>
2022-10-14 14:44:04 +0530

    First commit

---
+++ Break_AND_Continue/src/demo_break_continue.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+
+/**
+ * @author DELL
+ *
+ */
+public class demo_break_continue {
+
+	
+	public static void main(String[] args) {
+			
+			
+		         for(int i=1; i<=10; i++)
+		         {
+		        	 if(i == 1)
+		        	 {
+		        		 continue;   // break;
+		        	 }
+		        	 System.out.println(i);
+		         }
+			}
+		
+
+	}
+
+
