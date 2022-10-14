33cd303cd0ec2cf996178f33e4dd99412c5b2b34
DELL <DELL@Shetty>
2022-10-14 14:31:21 +0530

    First commit

---
+++ For_While_DoWhile_Club/src/demo_club.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+
+/**
+ * @author DELL
+ *
+ */
+public class demo_club {
+
+	
+	public static void main(String[] args) {
+		
+		
+		 // for loop
+		  for(int i=1,j=1; i<10||j<10;i++,j++)
+		  { 
+			  j++;
+			  System.out.println("i="+i+" and j="+j); 
+		  }
+
+
+		  // while loop 
+			  int a=1,b=1;
+			  while(a<10 || b<10)
+			  { 
+				  a++; b++;
+			      System.out.println("a="+a+" and b="+b); 
+			  }	
+			
+		// Do while loop
+			     int m = 1, n = 1;
+			do {
+				m++;
+				n++;
+				System.out.println("m=" + m + " and n=" + n);
+			    } while (m < 10 || n < 10);
+		}
+	
+
+	}
+
+
